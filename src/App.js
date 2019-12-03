b9097cfa495fa2bfdb971262c9e18d19143be4c5
Dournbrood <tanner1220002@gmail.com>
2019-12-03 16:31:32 -0500

    MVP on App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Axios from "axios";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    followers: [],
+  };
+
+  componentDidMount() {
+    Axios.get("https://api.github.com/users/Dournbrood")
+      .then(userResponse => {
+        console.log(userResponse);
+        Axios.get("https://api.github.com/users/Dournbrood/followers")
+          .then(followerResponse => {
+            console.log(followerResponse);
+            this.setState({
+              users: [...this.state.users, userResponse.data],
+              followers: [...this.state.followers, followerResponse.data],
+            });
+            console.log(this.state);
+          })
+          .catch(error => { });
+      })
+      .catch(error => { });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="header">
+            <p>React</p> <p>GitHub</p> <p>Cards</p>
+          </div>
+          <div className="cards">
+            {this.state.users.map((element) => (
+              <div className="card">
+                <img src={element.avatar_url} />
+                <div className="card-info">
+                  <h3 className="name">{element.name}</h3>
+                  <p className="username">{element.login}</p>
+                  <p>Location: {element.location}</p>
+                  <p>Followers ({element.following}): {this.state.followers[0].map((follower) => (<>{follower.login}, </>))} </p>
+                  <p>Following: {element.following}</p>
+                  <p>
+                    Profile:
+                    <a href={element.url}> {element.url}</a>
+                  </p>
+                  <p>Bio: {element.bio}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div >
+    );
+  }
 }
 
 export default App;
